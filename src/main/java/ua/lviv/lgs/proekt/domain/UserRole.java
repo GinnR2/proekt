92e43824201a71f0c360a2f09ee5f937e1c679a6
GinnR2 <pavlooo1212@gmail.com>
2020-04-13 21:02:16 +0300

    test

---
+++ src/main/java/ua/lviv/lgs/proekt/domain/UserRole.java
@@ -0,0 +1,5 @@
+package ua.lviv.lgs.proekt.domain;
+
+public enum UserRole {
+	ADMINISTRATOR, USER;
+}
---
+++ src/test/java/ua/lviv/lgs/proekt/Main.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs.proekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Main {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
